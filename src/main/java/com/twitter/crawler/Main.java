27896f2a1f9829ac63fad2ba0c3853513a7f8cc2
siavash <siavash.solimanii@yahoo.com>
2019-10-18 14:21:16 +0330

    initial commit

---
+++ src/main/java/com/twitter/crawler/Main.java
@@ -0,0 +1,27 @@
+package com.twitter.crawler;
+
+import twitter4j.*;
+import twitter4j.conf.ConfigurationBuilder;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws TwitterException, InterruptedException {
+        ConfigurationBuilder cb = new ConfigurationBuilder();
+        cb.setDebugEnabled(true)
+                .setOAuthConsumerKey("xxxxxxxxxxxxxxxxxxxxxxxxx")
+                .setOAuthConsumerSecret("xxxxxxxxxxxxxxxxxxxxxx")
+                .setOAuthAccessToken("xxxxxxxxxxxxxxxxxxxxxxxx")
+                .setOAuthAccessTokenSecret("xxxxxxxxxxxxxxxxxxxxxxxx");
+        TwitterFactory tf = new TwitterFactory(cb.build());
+        Twitter twitter = tf.getInstance();
+        ResponseList<Status> homeTimeline = twitter.getHomeTimeline(new Paging(1, 10000000));
+        System.out.println(homeTimeline.size());
+        for (Status status : homeTimeline) {
+            if (status.getText().contains("ام")) {
+                System.out.println(status.getText());
+            }
+        }
+
+    }
+}
